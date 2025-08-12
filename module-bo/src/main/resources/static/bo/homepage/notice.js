c0acdb0c66cc7b7d53d51f7eda8345b7440ea147
wsh <witmals6999@gmail.com>
2025-08-12 16:07:59 +0900

    반응형 페이지 수정_1

--- module-bo/src/main/resources/static/bo/homepage/notice.js
+++ module-bo/src/main/resources/static/bo/homepage/notice.js
@@ -1,15 +1,26 @@
-// 페이징
-$("a[data-role=btnGoPage]").click(function () {
-    let page = $(this).attr('data-page');
-    $("form[name=searchForm] input[name=page]").val(page);
-    $("form[name=searchForm]").submit();
+let searchDirty = false;
+
+$('select[name="searchType"], input[name="searchValue"]').on('input change', function () {
+    searchDirty = true;
 });
 
-//검색
-$('button[data-role=btn-search-form]').click(function(){
+// 검색 버튼: 무조건 1페이지부터
+$('button[data-role="btn-search-form"]').on('click', function () {
     $('input[name="page"]').val(1);
-    $('form[name=searchForm]').submit();
-})
+    searchDirty = false; // 제출했으니 초기화
+    $('form[name="searchForm"]').submit();
+});
+
+// 페이지 번호 클릭
+$(document).on('click', 'a[data-role="btnGoPage"]', function () {
+    const $form = $('form[name="searchForm"]');
+    const page = $(this).attr('data-page');
+
+    // 검색어/조건이 바뀐 상태라면 강제로 1페이지부터
+    $form.find('input[name="page"]').val(searchDirty ? 1 : page);
+    searchDirty = false; // 제출했으니 초기화
+    $form.submit();
+});
 
 //등록
 $('button[data-role=btnRegister]').click(function(){
--- module-bo/src/main/resources/static/bo/user/user.js
+++ module-bo/src/main/resources/static/bo/user/user.js
@@ -3,14 +3,28 @@ $('.search-form').keyup(function(event) {
         $('form[name=searchForm]').submit();
     }
 });
-$('button[data-role=btn-search-form]').click(function (){
-    $('input[name="page"]').val(1);
-    $('form[name=searchForm]').submit();
+let searchDirty = false;
+
+$('select[name="searchType"], input[name="searchValue"]').on('input change', function () {
+    searchDirty = true;
 });
-$('a[data-role=btnGoPage]').click(function () {
-    let page = $(this).attr('data-page');
-    $('form[name=searchForm] input[name=page]').val(page);
-    $('form[name=searchForm]').submit();
+
+// 검색 버튼: 무조건 1페이지부터
+$('button[data-role="btn-search-form"]').on('click', function () {
+    $('input[name="page"]').val(1);
+    searchDirty = false; // 제출했으니 초기화
+    $('form[name="searchForm"]').submit();
+});
+
+// 페이지 번호 클릭
+$(document).on('click', 'a[data-role="btnGoPage"]', function () {
+    const $form = $('form[name="searchForm"]');
+    const page = $(this).attr('data-page');
+
+    // 검색어/조건이 바뀐 상태라면 강제로 1페이지부터
+    $form.find('input[name="page"]').val(searchDirty ? 1 : page);
+    searchDirty = false; // 제출했으니 초기화
+    $form.submit();
 });
 
 // 수정/상세
